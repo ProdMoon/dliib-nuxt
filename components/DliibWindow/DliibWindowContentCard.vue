1c91a305c734cf2e1839d44fd4c892aacfb604a4
Junho Moon <112556572+ProdMoon@users.noreply.github.com>
2024-06-23 22:32:23 +0900

    feat: 좋았다 분발하자 버튼

--- components/DliibWindow/DliibWindowContentCard.vue
+++ components/DliibWindow/DliibWindowContentCard.vue
@@ -1,5 +1,20 @@
+<script setup lang="ts">
+const handleLikeClick = () => {
+  console.log('Like button clicked');
+};
+const handleTryHarderClick = () => {
+  console.log('Try harder button clicked');
+};
+</script>
+
 <template>
-  <div class="h-[95%] shadow-lg rounded-lg m-5 p-5 bg-gray-100 text-lg snap-start">
+  <div class="relative h-[95%] shadow-lg rounded-lg m-5 p-5 bg-gray-100 text-lg snap-start">
+    <div class="absolute bottom-20 right-6">
+      <div class="space-y-6">
+        <ButtonLike :onClick="handleLikeClick" />
+        <ButtonTryHarder :onClick="handleTryHarderClick" />
+      </div>
+    </div>
     <slot></slot>
   </div>
 </template>

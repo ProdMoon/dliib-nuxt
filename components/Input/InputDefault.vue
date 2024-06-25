b3959d9cbb96bec63d6ddc9eba08ecdf72000cb8
Junho Moon <112556572+ProdMoon@users.noreply.github.com>
2024-06-25 23:42:04 +0900

    feat: default input component

---
+++ components/Input/InputDefault.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const props = defineProps<{
+  type?: string;
+  id?: string;
+  class?: string;
+}>();
+
+const combinedClass = `px-4 py-2 rounded-md border bg-white text-black hover:bg-blue-50 focus:bg-blue-50 inline-block ${props.class ?? ''}`;
+</script>
+
+<template>
+  <input :type="props.type" :id="props.id" :class="combinedClass" />
+</template>

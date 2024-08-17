91534fab24330a0b72938f84c0146e825b3b45c1
Junho Moon <112556572+ProdMoon@users.noreply.github.com>
2024-08-17 17:36:47 +0900

    feat: 드립을 옆으로 넘겨보는 기능 [CARD-7], fix: 내가 쓴 글 스크롤 생기면 화면 밖으로 나가는 문제 [CARD-10]

--- components/DliibWindow/DliibWindowContentCard.vue
+++ components/DliibWindow/DliibWindowContentCard.vue
@@ -1,14 +1,46 @@
 <script setup lang="ts">
-import type { Dliib } from "@/types/dliib";
+import type { Dliib } from '@/types/dliib';
 
 const props = defineProps<{
-  dliib?: Dliib,
+  dliib?: Dliib;
 }>();
 
 const { status, token } = useAuth();
 const modalNeedLoginShow = useState('modalNeedLoginShow', () => false);
 
 const dliib = ref<Dliib | undefined>(props.dliib);
+const dliibContentElements = ref<HTMLElement[]>([]);
+const currentPage = ref(0);
+const maxPage = computed(() => {
+  const length = dliib.value?.contents?.length ?? 0;
+  return length > 0 ? length - 1 : 0;
+});
+
+onMounted(() => {
+  initObserver();
+  dliibContentElements.value = Array.from(document.getElementsByClassName('dliib-content')) as HTMLElement[];
+  dliibContentElements.value.forEach(el => observer.observe(el));
+});
+
+onUnmounted(() => {
+  observer.disconnect();
+});
+
+let observer: IntersectionObserver;
+const initObserver = () => {
+  if (observer) observer.disconnect();
+  observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const idx = dliibContentElements.value.indexOf(entry.target as HTMLElement);
+        if (idx !== -1) currentPage.value = idx;
+      }
+    });
+  }, {
+    root: document.querySelector('.dliib-contents-container'),
+    threshold: 0.5,
+  });
+};
 
 const handleLikeClick = async () => {
   if (!dliib.value) return;
@@ -42,7 +74,7 @@ const handleTryHarderClick = async () => {
       Authorization: token.value!,
     },
   });
-  
+
   await getDliib();
 };
 
@@ -54,22 +86,74 @@ const getDliib = async () => {
     },
   });
 };
+
+const moveToPreviousPage = () => {
+  const el = document.getElementById(`dliib-${dliib.value!.id}-${currentPage.value - 1}`);
+  if (el) el.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  currentPage.value -= 1;
+};
+
+const moveToNextPage = () => {
+  const el = document.getElementById(`dliib-${dliib.value!.id}-${currentPage.value + 1}`);
+  if (el) el.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  currentPage.value += 1;
+};
 </script>
 
+<style scoped>
+.dliib-contents-container {
+  height: calc(100% - 5rem);
+}
+</style>
+
 <template>
-  <div class="relative h-[95%] shadow-lg rounded-lg m-5 p-5 bg-gray-100 text-lg snap-start">
+  <div class="relative h-[95%] shadow-lg rounded-lg m-5 px-3 pt-3 bg-gray-200 text-lg snap-start">
     <template v-if="dliib">
-      <div v-html="dliib.content?.replaceAll('\n', '<br />')"></div>
+      <div class="dliib-contents-container overflow-x-auto h-full flex snap-x snap-mandatory">
+        <div
+          :id="`dliib-${dliib.id}-${idx}`"
+          class="dliib-content snap-center min-w-full overflow-y-auto border rounded-lg bg-gray-100 p-5"
+          v-for="(dliibContent, idx) in dliib.contents"
+          v-html="dliibContent?.replaceAll('\n', '<br />')"
+        ></div>
+      </div>
+      <ButtonDefault v-if="maxPage > 0 && currentPage > 0" @click="moveToPreviousPage" class="absolute left-0 top-1/2 -translate-y-1/2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="3"
+          stroke="currentColor"
+          class="size-6"
+        >
+          <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+        </svg>
+      </ButtonDefault>
+      <ButtonDefault v-if="maxPage > 0 && currentPage < maxPage" @click="moveToNextPage" class="absolute right-0 top-1/2 -translate-y-1/2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="3"
+          stroke="currentColor"
+          class="size-6"
+        >
+          <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+        </svg>
+      </ButtonDefault>
       <div class="absolute bottom-5 left-5 text-gray-500">
         <div class="italic">by. {{ dliib.authorNickName ?? '익명' }}</div>
         <div class="text-sm">{{ momentFormat(dliib.createdAt) }}</div>
       </div>
       <div class="absolute bottom-5 right-5 text-gray-500">
-        <div class="italic text-sm">좋았다 <span class="text-blue-500">{{ dliib.likes }}개</span> / 분발하자 <span class="text-red-500">{{ dliib.dislikes }}개</span></div>
+        <div class="italic text-sm">
+          좋았다 <span class="text-blue-500">{{ dliib.likes }}개</span> / 분발하자
+          <span class="text-red-500">{{ dliib.dislikes }}개</span>
+        </div>
       </div>
-      <div class="absolute bottom-20 right-6">
+      <div class="absolute bottom-24 right-7">
         <div class="space-y-6">
-          <ButtonLike :on-click="handleLikeClick" :is-clicked="dliib.isLiked"/>
+          <ButtonLike :on-click="handleLikeClick" :is-clicked="dliib.isLiked" />
           <ButtonTryHarder :on-click="handleTryHarderClick" :is-clicked="dliib.isDisliked" />
         </div>
       </div>
--- pages/dliib/hit.vue
+++ pages/dliib/hit.vue
@@ -11,8 +11,9 @@ const router = useRouter();
 
 const showModal = ref(false);
 const dliib: Ref<Dliib> = ref({
-  content: '',
+  contents: [''],
 });
+const contentsGarbage = ref<string[]>([]);
 
 const handleConfirm = async () => {
   const _ = await useApiFetch('/api/dliib', {
@@ -25,14 +26,91 @@ const handleConfirm = async () => {
   });
   router.push('/');
 };
+
+const addPage = () => {
+  const garbage = contentsGarbage.value.pop();
+  dliib.value.contents!.push(garbage ?? '');
+};
+
+const removePage = () => {
+  if (dliib.value.contents!.length === 1) return;
+  const content = dliib.value.contents!.pop();
+  if (content) contentsGarbage.value.push(content);
+};
+
+const getCurrentPage = () => {
+  const positions = [];
+  for (let i = 0; i < dliib.value.contents!.length; i++) {
+    const el = document.getElementById(`dliib-${i}`);
+    if (el) positions.push(el.getBoundingClientRect().left);
+  }
+  const idx = positions.findIndex((pos) => pos > 0);
+  return idx;
+};
+
+const moveToPreviousPage = () => {
+  const idx = getCurrentPage();
+  if (idx === -1) return;
+  const el = document.getElementById(`dliib-${idx - 1}`);
+  if (el) el.scrollIntoView({ behavior: 'smooth', block: 'center' });
+};
+
+const moveToNextPage = () => {
+  const idx = getCurrentPage();
+  if (idx === -1) return;
+  const el = document.getElementById(`dliib-${idx + 1}`);
+  if (el) el.scrollIntoView({ behavior: 'smooth', block: 'center' });
+};
 </script>
 
 <template>
   <div class="p-5">
-    <textarea v-model="dliib.content" class="w-full h-96 p-4 border rounded-lg" placeholder="여기에 당신의 드립 입력"></textarea>
-    <div class="flex justify-end mt-4">
-      <ButtonRound @click="showModal = true">끝!</ButtonRound>
+    <div class="relative">
+      <div class="overflow-x-auto flex snap-x snap-mandatory">
+        <div
+          :id="`dliib-${idx}`"
+          v-for="(_, idx) in dliib.contents"
+          class="snap-center min-w-full flex items-center justify-center"
+        >
+          <textarea
+            v-model="dliib.contents![idx]"
+            class="w-[94%] h-96 p-4 border rounded-lg"
+            placeholder="여기에 당신의 드립 입력"
+          ></textarea>
+        </div>
+      </div>
+      <ButtonDefault @click="moveToPreviousPage" class="absolute -left-2 top-1/2 -translate-y-1/2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-6"
+        >
+          <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+        </svg>
+      </ButtonDefault>
+      <ButtonDefault @click="moveToNextPage" class="absolute -right-2 top-1/2 -translate-y-1/2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="size-6"
+        >
+          <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+        </svg>
+      </ButtonDefault>
     </div>
+    <div class="flex justify-end space-x-2 mt-4">
+      <ButtonRound v-if="dliib.contents!.length > 1" @click="removePage">마지막 페이지 삭제</ButtonRound>
+      <ButtonRound @click="addPage">페이지 추가</ButtonRound>
+    </div>
+  </div>
+  <div class="absolute left-0 bottom-0 w-full p-4">
+    <ButtonRound @click="showModal = true" class="w-full" confirm>끝!</ButtonRound>
   </div>
   <ModalAreYouDone v-if="showModal" @close="showModal = false" @confirm="handleConfirm"></ModalAreYouDone>
 </template>

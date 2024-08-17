efc70282dffb6ad8a9d6fe09d0af5a025ec0b249
Junho Moon <112556572+ProdMoon@users.noreply.github.com>
2024-08-17 18:14:16 +0900

    fix: blank layout에 모바일 dvh 적용

--- layouts/blank.vue
+++ layouts/blank.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="flex justify-center items-center h-screen">
+  <main class="flex justify-center items-center h-dvh">
     <div class="w-full h-full max-w-screen-sm bg-gray-50 relative">
       <slot></slot>
     </div>
--- layouts/hasback.vue
+++ layouts/hasback.vue
@@ -7,12 +7,6 @@ const handleBack = () => {
 };
 </script>
 
-<style scoped>
-.container {
-  height: calc(100dvh - 3.5rem);
-}
-</style>
-
 <template>
   <NuxtLayout name="blank">
     <div class="h-14 bg-gray-200 flex items-center justify-center relative">
@@ -21,7 +15,7 @@ const handleBack = () => {
       </div>
       <div class="text-2xl font-bold">{{ route.meta.title }}</div>
     </div>
-    <div class="container overflow-y-auto">
+    <div class="h-full overflow-y-auto">
       <slot></slot>
     </div>
   </NuxtLayout>
